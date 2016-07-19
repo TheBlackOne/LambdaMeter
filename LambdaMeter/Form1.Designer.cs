b69c2b38edb34263bc71b547c4cf95b85838ae79
unknown <carsten.holbach@gmx.de>
2016-07-19 22:42:50 +0200

    - Added fast and start buttons - Added parsing for values - Added parsing for status an cj errors - Added parsing for reference and battery voltage

--- LambdaMeter/Form1.Designer.cs
+++ LambdaMeter/Form1.Designer.cs
@@ -28,10 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.refreshButton = new System.Windows.Forms.Button();
             this.openButton = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.fastButton = new System.Windows.Forms.Button();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.startButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -69,11 +74,33 @@
             this.textBox1.Size = new System.Drawing.Size(669, 20);
             this.textBox1.TabIndex = 3;
             // 
+            // fastButton
+            // 
+            this.fastButton.Location = new System.Drawing.Point(13, 73);
+            this.fastButton.Name = "fastButton";
+            this.fastButton.Size = new System.Drawing.Size(75, 23);
+            this.fastButton.TabIndex = 4;
+            this.fastButton.Text = "Fast Mode";
+            this.fastButton.UseVisualStyleBackColor = true;
+            this.fastButton.Click += new System.EventHandler(this.fastButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(12, 103);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 5;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(693, 497);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.fastButton);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.openButton);
             this.Controls.Add(this.refreshButton);
@@ -81,6 +108,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -92,6 +120,9 @@
         private System.Windows.Forms.Button refreshButton;
         private System.Windows.Forms.Button openButton;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button fastButton;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Button startButton;
     }
 }
 
--- LambdaMeter/Form1.cs
+++ LambdaMeter/Form1.cs
@@ -8,11 +8,42 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO.Ports;
+using System.Diagnostics;
 
 namespace LambdaMeter
 {
+    [Flags]
+    public enum Status
+    {
+        None = 0,
+        CJ_Error = 1 << 0,
+        Probe_Overheated = 1 << 1,
+        Ubat_Low = 1 << 2,
+        Ubat_High = 1 << 3,
+        SPI_Error = 1 << 4,
+        System_Ready = 1 << 5,
+        Watchdog = 1 << 6,
+        Calibration_Mode = 1 << 7
+    }
+
+    [Flags]
+    public enum CJ
+    {
+        None = 0,
+        VM1 = 1 << 0,
+        VM2 = 1 << 1,
+        UN1 = 1 << 2,
+        UN2 = 1 << 3,
+        IA_IP1 = 1 << 4,
+        IA_IP2 = 1 << 5,
+        DIAHD = 1 << 6,
+        DIAHG = 1 << 7
+    }
+
     public partial class Form1 : Form
     {
+        SerialPort serialPort;
+
         public Form1()
         {
             InitializeComponent();
@@ -28,11 +59,131 @@ namespace LambdaMeter
             }
         }
 
+        private decimal GetReferenceVoltage(int reference)
+        {
+            decimal result = 5.0m / 1023.0m * reference;
+            return Math.Round(result, 3);
+        }
+
+        private decimal GetBatteryVoltage(int ubat)
+        {
+            decimal result = 5.0m * 4.9m / 1023.0m * ubat;
+            return Math.Round(result, 3);
+        }
+
+        private string GetStatusMessage(Status status)
+        {
+            string result = string.Empty;
+
+            switch(status)
+            {
+                case Status.CJ_Error:
+                    result = "CJ error!";
+                    break;
+                case Status.Probe_Overheated:
+                    result = "Probe overheated!";
+                    break;
+                case Status.Ubat_Low:
+                    result = "Ubat low!";
+                    break;
+                case Status.Ubat_High:
+                    result = "Ubat high!";
+                    break;
+                case Status.SPI_Error:
+                    result = "SPI error!";
+                    break;
+                case Status.System_Ready:
+                    result = "System ready.";
+                    break;
+                case Status.Watchdog:
+                    result = "Watchdog! Restart controller!";
+                    break;
+                case Status.Calibration_Mode:
+                    result = "Calibration mode.";
+                    break;
+	        }
+
+            return result;
+        }
+
+        private string GetCJErrorString(CJ cj)
+        {
+            string result = string.Empty;
+
+            List<string> errorStrings = new List<string>();
+
+            if (!cj.HasFlag(CJ.DIAHG) && !cj.HasFlag(CJ.DIAHD))
+            {
+                errorStrings.Add("Short to ground!");
+            }
+            else if (cj.HasFlag(CJ.DIAHD))
+            {
+                errorStrings.Add("Heating not connected!");
+            }
+            else if (cj.HasFlag(CJ.DIAHG))
+            {
+                errorStrings.Add("Short to Ubat!");
+            }
+
+            if ((!cj.HasFlag(CJ.VM1) && !cj.HasFlag(CJ.VM2)) ||
+                (!cj.HasFlag(CJ.UN1) && !cj.HasFlag(CJ.UN2)) ||
+                (!cj.HasFlag(CJ.IA_IP1) && !cj.HasFlag(CJ.IA_IP2)))
+            {
+                errorStrings.Add("Short to ground!");
+            }
+            else if ((cj.HasFlag(CJ.VM1) && !cj.HasFlag(CJ.VM2))
+                   || (cj.HasFlag(CJ.UN1) && !cj.HasFlag(CJ.UN2))
+                   || (cj.HasFlag(CJ.IA_IP1) && !cj.HasFlag(CJ.IA_IP2)))
+            {
+                errorStrings.Add("Battery weak!");
+            }
+            else if ((!cj.HasFlag(CJ.VM1) && cj.HasFlag(CJ.VM2))
+                   || (!cj.HasFlag(CJ.UN1) && cj.HasFlag(CJ.UN2))
+                   || (!cj.HasFlag(CJ.IA_IP1) && cj.HasFlag(CJ.IA_IP2)))
+            {
+                errorStrings.Add("Short to Ubat!");
+            }
+
+            result = string.Join(" ", errorStrings);
+
+            return result;
+        }
+
+        private void ParseCSV(string csv)
+        {
+            string[] values = csv.Split(';');
+
+            if (values.Count() == 5)
+            {
+                int lambda = int.Parse(values[0]);
+                int reference = int.Parse(values[1]);
+                int bat = int.Parse(values[2]);
+                int status = int.Parse(values[3]);
+                int cj = int.Parse(values[4]);
+
+                decimal referenceVoltage = GetReferenceVoltage(reference);
+                decimal batteryVoltage = GetBatteryVoltage(bat);
+
+                string statusMessage = GetStatusMessage((Status)status);
+
+                string cjError = string.Empty;
+                if (cj < 255)
+                {
+                    cjError = GetCJErrorString((CJ)cj);
+                }
+
+                Debug.WriteLine(String.Format("lambda: {0}, reference: {1}, bat: {2}, status: {3}, cj: {4}", lambda, referenceVoltage, batteryVoltage, statusMessage, cjError));
+            }
+            else
+            {
+                // SendCSVModeCommand();
+            }
+        }
+
         private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
         {
             SerialPort sp = (SerialPort)sender;
-            string indata = sp.ReadExisting();
-            textBox1.Text = String.Format("Data Received: {0}", indata);
+            ParseCSV(sp.ReadLine());
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -47,20 +198,37 @@ namespace LambdaMeter
 
         private void openButton_Click(object sender, EventArgs e)
         {
-            string comPortName = comboBox1.SelectedText;
+            string serialPortName = comboBox1.SelectedItem.ToString();
 
-            SerialPort serialPort = new SerialPort(comPortName);
+            serialPort = new SerialPort(serialPortName);
 
             serialPort.BaudRate = 115200;
             serialPort.Parity = Parity.None;
             serialPort.StopBits = StopBits.One;
             serialPort.DataBits = 8;
             serialPort.Handshake = Handshake.None;
+            serialPort.NewLine = "\r\n";
             serialPort.RtsEnable = true;
 
             serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
 
             serialPort.Open();
+        }
+
+        private void fastButton_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.WriteLine("F");
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.WriteLine("H");
+            }
         }
     }
 }
